2b46c6bed1cbfbe364f0df328213d27c5a3c9886
lWarne <laurencewarne@gmail.com>
2021-05-16 13:42:59 +0100

    Attempt at test.

--- tests/emacs-fin-test.el
+++ tests/emacs-fin-test.el
@@ -8,7 +8,26 @@
 
 (require 'buttercup)
 (require 'emacs-fin)
+(require 'cl-lib)
 
 (describe "some test"
   (it "test foo"
     (expect (1+ 1) :to-equal 2)))
+
+(describe "fin-insert-book-data"
+          (it "test inserted data is reasonable"
+              (let ((fn-output))
+                (cl-letf* (((symbol-function 'insert)
+                            (lambda (string) (add-to-list 'fn-output string t)))
+                           ((symbol-function 'overlay-put) #'ignore))
+                  (fin-insert-book-data
+                   '((title . "Flowers for Algernon")
+                     (author . "Daniel Keyes")
+                     (description . "A description.")
+                     (image-file-name . "/some/random/image.png"))))
+                (print fn-output)
+                (expect fn-output :to-equal '("foo"))
+                ;; (expect (cl-search "flowers for algernon"
+                ;;                    (downcase (mapconcat #'identity fn-output "\n")))
+                ;;         :not :to-be nil)
+                )))
