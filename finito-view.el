LaurenceWarne/finito.el

commit c48962dc45434886dbf2355e86e81941df0ecc9b

Fix end of history error
There won't be any searches made initially so users will encounter
`End of history` transient error because of negated check

finito-view.el
@@ -105,7 +105,7 @@
 If OBJ has an empty value and `finito-save-last-search' is non-nil, switch
 to the last value used for OBJ."
   (transient--history-init obj)
-  (when (and finito-save-last-search (not (oref obj value)))
+  (when (and finito-save-last-search (oref obj value))
     (let ((transient--prefix obj))
       (transient-history-prev))))
 

commit d02eefdf0207177dedf3edb5a831447b0f235da1

Fix last search functionality
Caused by c48962d

finito-view.el
@@ -105,7 +105,9 @@
 If OBJ has an empty value and `finito-save-last-search' is non-nil, switch
 to the last value used for OBJ."
   (transient--history-init obj)
-  (when (and finito-save-last-search (oref obj value))
+  (when (and finito-save-last-search
+             (not (oref obj value))
+             (cdr (oref obj history)))
     (let ((transient--prefix obj))
       (transient-history-prev))))
 